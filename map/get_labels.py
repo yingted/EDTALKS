145d8ffcadea78d241cc8bcd5da8d9b527ab6ee2
Ted Ying <yingted@gmail.com>
2013-11-10 01:46:57 -0500

    rotations and coords

--- map/get_labels.py
+++ map/get_labels.py
@@ -10,32 +10,49 @@ def show(data):
 	lab.place(x=data['x'],y=data['y'])
 def add(e):
 	data={'x':e.x,'y':e.y,'name':askstring(argv[1],'destination')}
-	out.append(data)
+	out['points'].append(data)
 	show(data)
 def quit(e):
 	with open(argv[2],'w')as f:
 		dump(out,f)
 	root.destroy()
 def clear(e):
-	out[:]=[]
+	out['points'][:]=[]
 	for lab in labels:
 		lab.place_forget()
 		lab.destroy()
 	labels[:]=[]
-if __name__=="__main__":
-	im=Image.open(argv[1])
-	root=Tk()  
-	tkimage=ImageTk.PhotoImage(im)
-	Label(root,image=tkimage).pack() 
-	labels=[]
-	try:
-		with open(argv[2])as f:
-			out=load(f)
-	except IOError:
-		out=[]
-	for data in out:
+def showall():
+	for data in out['points']:
 		show(data)
-	root.bind("<Return>",add)
-	root.bind("<space>",quit)
-	root.bind("<Delete>",clear)
-	root.mainloop()
+tkimage=None
+def rotate(e,user=True):
+	global tkimage
+	if user:
+		out['rotate']=(out['rotate']+1)%4
+	im=Image.open(argv[1]).rotate(out['rotate']*90)
+	if user:
+		w,h=im.size
+		new=[{'x':pt['y'],'y':h-pt['x'],'name':pt['name']}for pt in out['points']]
+		clear(None)
+		out['points']=new
+		showall()
+	tkimage=ImageTk.PhotoImage(im)
+	lab.configure(image=tkimage)
+	lab.pack() 
+
+root=Tk()
+lab=Label(root)
+labels=[]
+try:
+	with open(argv[2])as f:
+		out=load(f)
+except IOError:
+	out={'rotate':0,'points':[]}
+rotate(None,False)
+showall()
+root.bind("<Return>",add)
+root.bind("<space>",quit)
+root.bind("<Delete>",clear)
+root.bind("r",rotate)
+root.mainloop()
